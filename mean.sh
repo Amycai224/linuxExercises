7e2290a9a2eae7f7b11253d7abfb19c951ea414b
Sijia Wu <swu447@wisc.edu>
2025-03-04 14:23:10 -0600

    Updated mean.sh

--- mean.sh
+++ mean.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+
+
+if [[ $# -lt 1 || $# -gt 2 ]]; then
+    echo "Usage: ./mean.sh <column> [file.csv]" 1>&2
+    exit 1
+fi
+column=$1
+file=${2:-/dev/stdin} 
+
+
+if ! [[ "$column" =~ ^[0-9]+$ ]] || ! [[ "$column" -ge 1 ]]; then
+    echo "Error: Column number must be a positive integer." 1>&2
+    exit 1
+fi
+
+
+tail -n +2 "$file" | cut -d ',' -f "$column" | awk '
+{
+    sum += $1
+    count += 1
+}
+END {
+    if (count > 0) {
+        print sum / count
+    } else {
+        print "Error: No valid data found." > "/dev/stderr"
+        exit 1
+    }
+}'
