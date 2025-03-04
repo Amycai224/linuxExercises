4672a55639323409d65c2881de4e9a3e14481389
Justina Jing <mjing4@w\isc.edu.com>
2025-03-04 09:12:10 -0600

    Added rm_n.sh script

--- rm_n.sh
+++ rm_n.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <dir> <n>" 1>&2
+  exit 1
+fi
+
+dir=$1
+n=$2
+
+echo "Directory passed: $dir"
+
+# check if the <dir> exists
+if [ ! -d "$dir" ]; then
+  echo "Error: Directory $dir does not exist." 1>&2
+  exit 1
+fi
+
+find "$dir" -type f -size +"${n}"c -exec rm {} \;
+echo "All files in $dir larger than $n bytes deleted."
