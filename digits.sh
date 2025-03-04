d2ab3d2c9371f14a9c6c618fa922d576ed8cc28e
Justina Jing <mjing4@w\isc.edu.com>
2025-03-04 08:52:43 -0600

    Updated digits.sh and added five_dirs.sh

--- digits.sh
+++ digits.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+sum=0
+
+for num in {1000..2000}; do if [[ $num =~ ^[01]+$ ]]; then
+sum=0
+
+for num in {1000..2000}; do
+  if [[ $num =~ ^[01]+$ ]]; then
+    sum=$((sum + num))
+  fi
+done
+
+echo "The sum of numbers with digits only from {0, 1} is: $sum"    sum=$((sum + num))
+  fi
+done
+
+echo "The sum of numbers with digits only from {0, 1} is: $sum"
