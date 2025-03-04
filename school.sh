c1bf06cc3c2d6e7863c433f04cb90f342bf582d4
Amy Cai <cai224@wisc.edu>
2025-03-04 08:58:29 -0600

    school.sh

--- school.sh
+++ school.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+
+cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d',' -f7 | {
+    sum=0
+    count=0
+    while read num; do
+        sum=$((sum + num))
+        count=$((count + 1))
+    done
+    if [[ $count -gt 0 ]]; then
+        echo "Sum: $sum"
+        echo "Average: $((sum / count))"
+    fi
+}
